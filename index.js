1b53aba3ef460abf839f0e5373b4cab31392eb1e
smokefactorybbq-star <smokefactorybbq@gmail.com>
2026-08-10 06:08:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 // https://xxxx.ngrok-free.app
 const GRAB_RECEIVER_URL = String(
   process.env.GRAB_RECEIVER_URL ||
-    "https://b1d3-171-6-240-2.ngrok-free.app"
+    https://741e-171-6-240-180.ngrok-free.app"
 ).trim();
 
 if (!BOT_TOKEN) throw new Error("BOT_TOKEN is not set");
